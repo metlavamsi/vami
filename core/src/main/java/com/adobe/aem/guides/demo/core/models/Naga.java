3993d3d14a406b22d8ff8a4994d40bf31ecc34b3
metlavamsi <metlavamsi2@gmail.com>
2024-12-03 11:48:57 +0530

    welcome

--- core/src/main/java/com/adobe/aem/guides/demo/core/models/Naga.java
+++ core/src/main/java/com/adobe/aem/guides/demo/core/models/Naga.java
@@ -4,8 +4,10 @@ import javax.annotation.Resource;
 
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
 @Model(adaptables = Resource.class,defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class Naga {
-    
+    @ValueMapValue
+    private String trext;
 }
