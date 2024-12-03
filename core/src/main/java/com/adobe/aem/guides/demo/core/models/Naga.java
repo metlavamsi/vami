0c805310220c40b79403b9ec1b1fc81dcc4adde4
metlavamsi <metlavamsi2@gmail.com>
2024-12-03 11:31:31 +0530

    commit new code

--- core/src/main/java/com/adobe/aem/guides/demo/core/models/Naga.java
+++ core/src/main/java/com/adobe/aem/guides/demo/core/models/Naga.java
@@ -1,5 +1,11 @@
 package com.adobe.aem.guides.demo.core.models;
 
+import javax.annotation.Resource;
+
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+
+@Model(adaptables = Resource.class,defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class Naga {
     
 }
