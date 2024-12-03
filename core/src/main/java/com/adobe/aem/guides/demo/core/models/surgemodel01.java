95169f9358abf69aeca25a87baf5d4a30f247ef7
metlavamsi <metlavamsi2@gmail.com>
2024-12-03 20:16:34 +0530

    vamsi

--- core/src/main/java/com/adobe/aem/guides/demo/core/models/surgemodel01.java
+++ core/src/main/java/com/adobe/aem/guides/demo/core/models/surgemodel01.java
@@ -1,5 +1,10 @@
 package com.adobe.aem.guides.demo.core.models;
 
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+
+@Model(adaptables = Resource.class,defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class surgemodel01 {
 
 }
